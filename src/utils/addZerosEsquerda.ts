48961da8d453332dbec193c5ac874227996ebdf2
Jefferson Araújo <jeffersonc201212@gmail.com>
2023-07-13 11:00:04 -0300

    Add: utilitário que adiciona zeros a esquerda de numeros e retorna como string criado.

---
+++ src/utils/addZerosEsquerda.ts
@@ -0,0 +1,11 @@
+export default function addZerosEsqueda(num: number, len: number) {
+  let numberWithZeroes = String(num);
+  let counter = numberWithZeroes.length;
+
+  while (counter < len) {
+    numberWithZeroes = '0' + numberWithZeroes;
+    counter++;
+  }
+
+  return numberWithZeroes;
+}
